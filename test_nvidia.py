eea178b37a6a9dec74168c4402abfb4c5469e58f
Amaan <bilwarad@mail.uc.edu>
2024-11-09 13:40:10 -0500

    add: test cases for nvidia nim

---
+++ test_nvidia.py
@@ -0,0 +1,12 @@
+import unittest
+from unittest.mock import patch, mock_open
+import nvidia
+import os
+
+
+class TestNvidia(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()

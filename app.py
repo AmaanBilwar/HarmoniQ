cd00a86806a990f4299e1e0c1a02cdd815936207
Amaan <bilwarad@mail.uc.edu>
2024-11-09 12:43:30 -0500

    refactor: functions

--- app.py
+++ app.py
@@ -1,41 +1,45 @@
 import cv2 
-import time 
+import os 
+
+def make_dir():
+    try:
+        os.mkdir('images')
+    except FileExistsError:
+        pass
 
 
-cap = cv2.VideoCapture(0)
 
-if not cap.isOpened():
-    print("Error: Could not open camera.")
-    exit()
+def capture_image():
+    cap = cv2.VideoCapture(0)
 
-countdown = 5
+    if not cap.isOpened():
+        print("Error: Could not open camera.")
+        return
 
-while countdown > 0:
-    #Capture frame-by-frame
+    countdown = 5
+
+    while countdown > 0:
+        ret, frame = cap.read()
+        
+        if not ret:
+            print("Error: Cannot receive frame.")
+            break
+        
+        font = cv2.FONT_HERSHEY_COMPLEX
+        cv2.putText(frame, str(countdown), (200, 400), font, 5, (255, 255, 255), 5, cv2.LINE_AA)
+        cv2.imshow('Camera feed', frame)
+        
+        cv2.waitKey(1000)
+        countdown -= 1
+        
     ret, frame = cap.read()
-    
-    if not ret:
-        print("Error: Cannot receive frame.")
-        break
-    
-    #Display the countdown on the frame/window
-    font = cv2.FONT_HERSHEY_COMPLEX
-    cv2.putText(frame, str(countdown), (200, 400), font, 5, (255, 255, 255), 5, cv2.LINE_AA)
-    cv2.imshow('Camera feed', frame)
-    
-    cv2.waitKey(1000)
-    countdown -= 1
-    
-# capture the final frame
-ret, frame = cap.read()
-if ret:
-    cv2.imwrite('captured_image.jpg', frame)
-    print("Image captured and saved as 'captured_image.jpg'")
+    if ret:
+        make_dir()
+        cv2.imwrite('images/captured_image.jpg', frame)
+        print("Image captured and saved as 'captured_image.jpg'")
 
-# Display the captured image for a few seconds
-cv2.imshow('Captured Image', frame)
-cv2.waitKey(3000)
+    cv2.imshow('Captured Image', frame)
+    cv2.waitKey(3000)
 
-# When everything is done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
